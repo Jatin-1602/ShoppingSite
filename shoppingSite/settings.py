839eaebceac56f34356adde989ca2af2d8f61c25
Jatin Gupta <jatingupta6171@gmail.com>
2023-11-24 11:01:46 +0530

    Update settings.py

--- shoppingSite/settings.py
+++ shoppingSite/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-2u9&m*)us5g4m+m8k&imd^c!_spd05lo#x!n+&vcex0#5u0%3k'
+SECRET_KEY = 'YOUR_SECRET_KEY'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
